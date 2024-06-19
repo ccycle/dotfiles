a9b91b8d7e1de79c4e218a2a3b3233d226449cf9
ccycle <ccycle713@gmail.com>
2024-06-19 22:32:51 +0900

    Remove ghcup

--- modules/common/haskell/ghcup.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ghcup, ... }:
-# let ghcupPackage = pkgs.callPackage ./ghcup { inherit ghcup; }; in
-{
-  # home.packages = [ ghcupPackage ];
-  programs.zsh.initExtra = ''
-    [ -f "$HOME/.ghcup/env" ] && source "$HOME/.ghcup/env"
-  '';
-}
--- modules/common/haskell/ghcup/default.nix
+++
@@ -1,6 +0,0 @@
-{ stdenv, ghcup }:
-stdenv.mkDerivation {
-  name = "ghcup";
-  src = ghcup;
-  installPhase = "sh ./scripts/bootstrap/bootstrap-haskell";
-}
