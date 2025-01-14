62e9682e03abdbc614fcf95cada2f3197aa197d6
ccycle <ccycle713@gmail.com>
2025-01-14 23:01:01 +0900

    Include HLS

--- modules/common/haskell.nix
+++ modules/common/haskell.nix
@@ -3,7 +3,7 @@
   imports = [
     # ./haskell/ghcup.nix
     # ./haskell/compiler.nix
-    # ./haskell/haskell-language-server.nix
+    ./haskell/haskell-language-server.nix
     ./haskell/cabal.nix
     ./haskell/dev-tools.nix
     ./haskell/stack.nix
