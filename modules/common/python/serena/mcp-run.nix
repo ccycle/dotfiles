55bcaae212b84e6d725532932d32e604f53f7e45
ccycle <ccycle713@gmail.com>
2025-08-03 03:41:14 +0900

    Add serena

---
+++ modules/common/python/serena/mcp-run.nix
@@ -0,0 +1,12 @@
+{ serena, ... }: # arguments
+{
+  config,
+  self,
+  inputs,
+  ...
+}:
+ {
+  programs.zsh.shellAliases = {
+    serena-mcp-server-run = "uv run serena-mcp-server --directory ${serena}";
+  };
+}
