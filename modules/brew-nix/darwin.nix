b892022b8e4172d8cd2641d3f96ca7250de3c465
ccycle <ccycle713@gmail.com>
2026-03-24 23:54:35 +0900

    refactor(brew-nix): move brew cask packages from home.nix to darwin.nix
    Move desktoppr and appcleaner to environment.systemPackages and add
    google-japanese-ime and betterdisplay. Remove now-empty home.nix.

--- modules/brew-nix/darwin.nix
+++ modules/brew-nix/darwin.nix
@@ -2,4 +2,32 @@
   brew-nix.enable = true;
   imports = [ inputs.brew-nix.darwinModules.default ];
   home-manager.sharedModules = [ ./home.nix ];
+
+  environment.systemPackages = [
+    (pkgs.brewCasks.desktoppr.overrideAttrs (o: {
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out/bin
+        cp ./usr/local/bin/desktoppr $out/bin/
+        runHook postInstall
+      '';
+    }))
+    pkgs.brewCasks.appcleaner
+    (pkgs.brewCasks.google-japanese-ime.overrideAttrs (old: {
+      nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.p7zip ];
+      src = old.src.overrideAttrs {
+        outputHash = "sha256-749fuw3zrpNGDt0/taaFtwnENd0vpwsKphhHMmbDSYw=";
+      };
+      unpackPhase = ''
+        7z x $src -o_dmg
+        for pkg in _dmg/**/*.pkg; do
+          xar -xf "$pkg"
+        done
+        for payload in $(cat Distribution | grep -oE "#.+\.pkg" | sed -e "s/^#//" -e "s/$/\/Payload/"); do
+          zcat "$payload" | cpio -i
+        done
+      '';
+    }))
+    pkgs.brewCasks.betterdisplay
+  ];
 }
