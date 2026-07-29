2a0915578aac9e09f0e5f32fabf64809ab8fdca4
ccycle <ccycle713@gmail.com>
2026-07-29 21:01:11 +0900

    feat(attic): enable bootstrap substituter configuration
    Uncomment and activate the Attic binary cache substituter in bootstrap.
    PUBLIC_KEY placeholder to be filled after init.sh is run on mac-mini-m4-pro.

--- bootstrap/modules/attic/darwin.nix
+++ bootstrap/modules/attic/darwin.nix
@@ -1,15 +1,15 @@
-{ ... }:
+{ config, lib, ... }:
 
 {
   # Attic substituter configuration for all machines.
   #
-  # After atticd is running and a cache has been created on mac-mini-m4-pro,
-  # uncomment and fill in the values below:
+  # After init.sh has been run and atticd is serving on mac-mini-m4-pro,
+  # fill in the public key below:
   #
   #   ssh mac-mini-m4-pro -- attic cache info dotfiles
   #
-  # nix.settings = {
-  #   extra-substituters = [ "https://cache.mac-mini-m4-pro.internal" ];
-  #   trusted-public-keys = [ "dotfiles:<PUBLIC_KEY>" ];
-  # };
+  nix.settings = {
+    extra-substituters = [ "https://cache.mac-mini-m4-pro.internal" ];
+    trusted-public-keys = [ "dotfiles:<PUBLIC_KEY>" ];
+  };
 }
