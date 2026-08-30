6011df3a87d77c1af1bf1d7f3ee7fd8fa32d6ced
ccycle <ccycle713@gmail.com>
2026-08-30 13:08:10 +0900

    feat(forgejo): add forgejo-cli (fj) for remote CLI access

---
+++ modules/forgejo/cli/home.nix
@@ -0,0 +1,5 @@
+{ pkgs, ... }:
+
+{
+  home.packages = [ pkgs.forgejo-cli ];
+}
--- modules/forgejo/home.nix
+++ modules/forgejo/home.nix
@@ -1,6 +1,10 @@
 { ... }:
 
 {
+  imports = [
+    ./cli/home.nix
+  ];
+
   programs.zsh.shellAliases = {
     forgejo-logs = "sudo tail -f /var/log/forgejo.log";
     forgejo-docker = "docker compose -p forgejo logs -f --tail=100";
