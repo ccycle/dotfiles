d40e0f864cf245ed4167811cea0beccc9bd0b7e2
ccycle <ccycle713@gmail.com>
2026-09-04 11:58:54 +0900

    chore(brew-nix): remove google-japanese-ime cask
    The custom .pkg unpack override doesn't extract DictionaryTool.app or
    ConfigDialog.app from the payload, leaving dangling symlinks that abort
    darwin-rebuild activation (discovered via vm-verify).

--- modules/brew-nix/darwin.nix
+++ modules/brew-nix/darwin.nix
@@ -13,21 +13,6 @@
       '';
     }))
     pkgs.brewCasks.appcleaner
-    (pkgs.brewCasks.google-japanese-ime.overrideAttrs (old: {
-      nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.p7zip ];
-      src = old.src.overrideAttrs {
-        outputHash = "sha256-749fuw3zrpNGDt0/taaFtwnENd0vpwsKphhHMmbDSYw=";
-      };
-      unpackPhase = ''
-        7z x $src -o_dmg
-        for pkg in _dmg/**/*.pkg; do
-          xar -xf "$pkg"
-        done
-        for payload in $(cat Distribution | grep -oE "#.+\.pkg" | sed -e "s/^#//" -e "s/$/\/Payload/"); do
-          zcat "$payload" | cpio -i
-        done
-      '';
-    }))
     pkgs.brewCasks.betterdisplay
     pkgs.brewCasks.zotero
   ];
