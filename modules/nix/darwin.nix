e18b89132dce02f9f9acd8f189965fd47b56e0e3
ccycle <ccycle713@gmail.com>
2026-09-04 22:40:44 +0900

    chore(nix): enable auto-optimise-store

--- modules/nix/darwin.nix
+++ modules/nix/darwin.nix
@@ -14,6 +14,7 @@
   nix.nixPath = lib.mkForce [ "nixpkgs=${inputs.nixpkgs}" ];
   nix.channel.enable = false;
   nix.settings.nix-path = lib.mkForce [ "nixpkgs=${inputs.nixpkgs}" ];
+  nix.settings.auto-optimise-store = true;
 
   nix.settings.trusted-users = [ username ];
 }
